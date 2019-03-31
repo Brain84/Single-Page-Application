18ab728e46f4646a209b6c543a7b6a5468ec8dd8
Pawel Tatar <pawel.tatar.email@gmail.com>
2019-03-31 17:21:19 +0200

    SPA

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import '../styles/Header.scss'
+import '../styles/Header.scss';
 
 const Header = () => {
   return (
     <header className="app__header">
-      <h1 className="header__title">The sky's limit</h1>
+      <h1 className="header__title">The sky&#39;s limit</h1>
       <p className="header__description">Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
     </header>
   );
